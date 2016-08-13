35d846a95c71b3bdc8913a6a11a730f064e51e5e
uroboro <uroboro845@gmail.com>
2016-08-13 18:26:39 -0300

    Added cydget template

---
+++ cydget/@@PROJECTNAME@@.mm
@@ -0,0 +1,73 @@
+@interface SBAwayViewPluginController : NSObject
+@end
+
+@interface @@PROJECTNAME@@LockScreenController : SBAwayViewPluginController
+@end
+
+@interface @@PROJECTNAME@@LockScreenView : UIView {
+}
+@end
+
+@implementation @@PROJECTNAME@@LockScreenController
+
++ (id) rootViewController {
+    return [[[self alloc] init] autorelease];
+}
+
+- (void) loadView {
+    [self setView:[[[@@PROJECTNAME@@LockScreenView alloc] init] autorelease]];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    
+}
+
+-(void)viewWillDisappear:(BOOL)animated {
+    
+}
+
+-(void)viewDidDisappear:(BOOL)animated {
+    
+}
+
+// Return YES if you handle the event
+-(BOOL)handleMenuButtonTap {
+    return NO;
+}
+-(BOOL)handleMenuButtonDoubleTap {
+    return NO;
+}
+-(BOOL)handleMenuButtonHeld {
+    return NO;
+}
+-(BOOL)handleGesture:(int)gestureType fingerCount:(unsigned)count {
+    return NO;
+}
+
+// Return NO to hide the lockscreen clock
+-(BOOL)showAwayItems {
+    return YES;
+}
+
+@end
+
+@implementation @@PROJECTNAME@@LockScreenView
+
+- (id) init {
+    CGRect frame = [[UIScreen mainScreen] bounds];
+    if ((self = [super initWithFrame:frame])) {
+        // Initialize stuff
+    }
+    return self;
+}
+
+- (void)dealloc {
+    
+    [super dealloc];
+}
+
+@end 
