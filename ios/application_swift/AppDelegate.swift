dee19bf979c62a9df06472738d173be04430a392
kabiroberai <oberai.kabir@gmail.com>
2018-01-25 19:27:36 +0530

    [application_swift] Force-unwrap window

--- ios/application_swift/AppDelegate.swift
+++ ios/application_swift/AppDelegate.swift
@@ -7,8 +7,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		window = UIWindow(frame: UIScreen.main.bounds)
-		window?.rootViewController = UINavigationController(rootViewController: RootViewController())
-		window?.makeKeyAndVisible()
+		window!.rootViewController = UINavigationController(rootViewController: RootViewController())
+		window!.makeKeyAndVisible()
 		return true
 	}
 
