a9d71c83d71a25093b34364fbb827d39527309bd
Leptos <leptos.0.null@gmail.com>
2018-07-22 12:33:16 -0700

    Converted control.pl to KILL_PROCS

--- ios/activator_event/NIC/control.pl
+++ ios/activator_event/NIC/control.pl
@@ -1,10 +1,8 @@
 my $default_kill = "SpringBoard";
 
-NIC->variable("KILL_RULE") = "";
+NIC->variable("KILL_PROCS") = "";
 
 my $kill_apps = NIC->prompt("KILL_APPS", "List of applications to terminate upon installation (space-separated, '-' for none)", {default => $default_kill});
 if($kill_apps ne "-") {
-	my @apps = split(/\s+/, $kill_apps);
-	my @commands = map {"killall -9 $_"} @apps;
-	NIC->variable("KILL_RULE") = "after-install::\n\tinstall.exec \"".join("; ", @commands)."\"";
+	NIC->variable("KILL_PROCS") = "INSTALL_TARGET_PROCESSES = ".$kill_apps;
 }
