82ece5adc09daacff16a5a12ab9839dc2d140c32
Leptos <leptos.0.null@gmail.com>
2018-07-22 18:51:45 -0700

    Moved KILL_PROCS location within Makefile from top to under PROJECTNAME variables

--- ios/activator_event/NIC/control.pl
+++ ios/activator_event/NIC/control.pl
@@ -4,5 +4,5 @@ NIC->variable("KILL_PROCS") = "";
 
 my $kill_apps = NIC->prompt("KILL_APPS", "List of applications to terminate upon installation (space-separated, '-' for none)", {default => $default_kill});
 if($kill_apps ne "-") {
-	NIC->variable("KILL_PROCS") = "INSTALL_TARGET_PROCESSES = ".$kill_apps;
+	NIC->variable("KILL_PROCS") = "INSTALL_TARGET_PROCESSES = ".$kill_apps."\n";
 }
