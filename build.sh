365bb8bb4aa5a995965e1107e10df4cf5c549181
uroboro <uroboro.845@gmail.com>
2019-06-02 23:28:28 +0200

    [build] Improve search logic for building nic templates.

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-for f in ./*; do
-	if [[ -d $f ]]; then
-		nicify.pl $f
+# For each directory containing a valid NIC template source folder
+templateDirs=$(find . -type d -name NIC -exec dirname {} \; | xargs -n1 dirname | sort -u)
+for d in $templateDirs; do
+	if [[ -d $d ]]; then
+		pushd $d &> /dev/null
+		# For each valid NIC template source folder
+		templateSubDirs=$(find . -type d -name NIC -exec dirname {} \;)
+		for f in $templateSubDirs; do
+			if [[ -d $f ]]; then
+				# Build the template
+				nicify.pl $f
+			fi
+		done
+		popd &> /dev/null
 	fi
 done
-
-sudo cp -t $THEOS/templates/iphone/ iphone_*
