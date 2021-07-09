c3ebae3a97e76d2fb6ffc5e53902b25247f20d52
ArtikusHG <artiomoleynic@gmail.com>
2021-07-09 15:03:52 +0300

    Add XPC service template with usage of Objective-C XPC API instead of the C API

---
+++ ios/xpc_service_modern/@@CLASSPREFIX@@XPCProtocol.h
@@ -0,0 +1,4 @@
+@protocol @@CLASSPREFIX@@XPCProtocol
+// Define all your methods here and implement them in the @@CLASSPREFIX@@XPC class
+- (void)lowercaseStringForString:(NSString *)string reply:(void (^)(NSString *))reply;
+@end
