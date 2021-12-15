6082897b47b1fb5a69a730571a327b7507933854
luki120 <luki120@exerra.xyz>
2021-12-15 20:56:20 -0300

    forgot AppDelegate.m

--- ios/application/@@CLASSPREFIX@@AppDelegate.m
+++ ios/application/@@CLASSPREFIX@@AppDelegate.m
@@ -3,11 +3,12 @@
 
 @implementation @@CLASSPREFIX@@AppDelegate
 
-- (void)applicationDidFinishLaunching:(UIApplication *)application {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	_window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
 	_rootViewController = [[UINavigationController alloc] initWithRootViewController:[[@@CLASSPREFIX@@RootViewController alloc] init]];
 	_window.rootViewController = _rootViewController;
 	[_window makeKeyAndVisible];
+	return YES;
 }
 
 @end
