2188ce9127197028ad736519e28e71ebf072a619
Donato Fiore <50346119+donato-fiore@users.noreply.github.com>
2022-07-05 10:30:56 -0400

    Update files to match kirb's requests

--- ios/safari_extension/SafariWebExtensionHandler.swift
+++ ios/safari_extension/SafariWebExtensionHandler.swift
@@ -5,15 +5,15 @@ let SFExtensionMessageKey = "message"
 
 class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
 
-    func beginRequest(with context: NSExtensionContext) {
-        let item = context.inputItems[0] as! NSExtensionItem
-        let message = item.userInfo?[SFExtensionMessageKey]
+	func beginRequest(with context: NSExtensionContext) {
+		let item = context.inputItems[0] as! NSExtensionItem
+		let message = item.userInfo?[SFExtensionMessageKey]
 		Logger().log("Received message from browser.runtime.sendNativeMessage: \(String(describing: message))")
 
-        let response = NSExtensionItem()
-        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+		let response = NSExtensionItem()
+		response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
 
-        context.completeRequest(returningItems: [response], completionHandler: nil)
-    }
+		context.completeRequest(returningItems: [response], completionHandler: nil)
+	}
 
 }
