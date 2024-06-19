a71a516535ab829e610bf5a76ecfb984a42a6784
Leptos <leptos.0.null@gmail.com>
2024-06-19 11:08:31 -0700

    Update mktemp calls to support GNU version

--- integrity_check.sh
+++ integrity_check.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-WORKSPACE_DIR=$(mktemp -d -t templates)
+WORKSPACE_DIR=$(mktemp -d --tmpdir='' "templates.XXXXXXXXXX")
 TEMPLATES_DIR=$(pwd)
 
 # For each directory containing a valid NIC templates' source
@@ -15,7 +15,7 @@ EXIT_CODE=0
 for TEMPLATES_SOURCE in $TEMPLATES_SOURCES; do
     "${THEOS}/bin/nicify.pl" "${TEMPLATES_SOURCE}" 2> /dev/null
     RESULT_FILE=$(ls *.nic.tar)
-    EXTRACT_DIR=$(mktemp -d -t "${RESULT_FILE}" -p .)
+    EXTRACT_DIR=$(mktemp -d -p . "${RESULT_FILE}.XXXXXXXXXX")
     tar -xf "${TEMPLATES_DIR}/${RESULT_FILE}" -C "${EXTRACT_DIR}"
     # thanks to https://stackoverflow.com/a/49633090
     if ! diff -r --no-dereference --brief -x '.keep' "${TEMPLATES_SOURCE}" "${EXTRACT_DIR}"; then
@@ -26,7 +26,10 @@ for TEMPLATES_SOURCE in $TEMPLATES_SOURCES; do
             echo "::error file=${RESULT_FILE},title=Mismatch::The NIC file does not match the template directory"
         fi
     fi
+    rm -r "${EXTRACT_DIR}"
     rm "${RESULT_FILE}"
 done
+
+rm -r "${WORKSPACE_DIR}"
 
 exit "${EXIT_CODE}"
