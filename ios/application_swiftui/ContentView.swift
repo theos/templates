f0fb942c4bb90ef331126e2b38257974b18bc895
Donato Fiore <50346119+donato-fiore@users.noreply.github.com>
2024-11-23 18:11:47 -0500

    Update SwiftUI application to match UI of other templates (#29)
    Co-authored-by: Leptos <leptos.0.null@gmail.com>

--- ios/application_swiftui/ContentView.swift
+++ ios/application_swiftui/ContentView.swift
@@ -1,8 +1,39 @@
 import SwiftUI
 
 struct ContentView: View {
-	var body: some View {
-		Text("Hello, world!")
-			.padding()
-	}
+    @State private var items: [Date] = []
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(items, id: \.self) { item in
+                        Text(item.description)
+                    }
+                    .onDelete(perform: delete)
+                }.toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
+                    }
+                }
+                .listStyle(InsetGroupedListStyle())
+            }
+            .navigationTitle("Content View")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        withAnimation(.easeInOut) {
+                            items.insert(Date(), at: 0)
+                        }
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        }
+    }
+
+    func delete(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
+    }
 }
